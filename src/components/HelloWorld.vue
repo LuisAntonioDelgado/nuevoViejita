56075dbd6db39b23b1beec3236ab84cf73cc23bb
Luis Antonio LADO. Delgado Osorio <luis.delgado@stkprojects.com>
2020-10-12 17:53:04 -0500

    Primero

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,104 +1,178 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <header class="side-header">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">
+                    <img src="../../public/img/logo.png" alt="Logotipo de Bienes Raices" style="width:5%;">
+                </a>
+                
+                <nav class="navegacion">
+                    <a href="nosotros.html">Metas</a>
+                    <a href="anuncios.html">Cupones</a>
+                    <a href="blog.html">Viajes</a>
+                    <a href="contacto.html">Canciones</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section class="side-header inicio">
+      <div class="contenedor contenido-header">
+      </div>
+    </section>
+        <!-- Newsletter -->
+    <section class="bg-dark text-white py-5">
+        <div class="container">
+            <div class="row " >
+                <div class="col-md-12 text-center display-4">
+                   Metas por cumplir
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Features  -->
+    <section class=" py-1 mt-5 mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3" >
+                    <div class="card text-center border-primary" style="height:15rem;">
+                        <div class="card-body custom-cardT">
+                            <h3 style="font-size: 3rem;">Vivir juntos</h3>
+                            <p style="font-size: 1.5rem;">
+                                Despertar cada dia a tu lado y ver esa sonrisa que ilumina mis dias.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-center bg-primary text-white" style="height:15rem;">
+                        <div class="card-body custom-cardT">
+                            <h3 style="font-size: 3rem;">Viajar Juntos</h3>
+                            <p style="font-size: 1.5rem;">
+                                Hay miles de cosas nuevas afuera y quiero vivirlas caminando de tu mano.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-center border-primary" style="height:15rem;">
+                        <div class="card-body custom-cardT">
+                            <h4 style="font-size: 3rem;">Casarnos</h4>
+                            <p style="font-size: 1.5rem;">
+                                Hacer formal nuestro amor ante los ojos de dios.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-center bg-primary text-white" style="height:15rem;">
+                        <div class="card-body custom-cardT">
+                            <h3 style="font-size: 3rem;">Ser Felices</h3>
+                            <p style="font-size: 1.5rem;">
+                                Siempre dara mi alma entera para verte sonreir y que no se te olvide, que cada dia amanezco màs enamorado de ti
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-dark text-white  py-5 mt-5">
+      <div class="container">
+          <div class="row " >
+              <div class="col-md-12 text-center display-4">
+                Cuponera de amor
+                <p style="font-size:2rem" class="pt-2">Esta parte es solo tuya mi princesa, en donde yo humildemente te ofrecere distintos paquetes, los cuales cuentan como cupones y podran ser canjeados cuando tu quieras.
+                </p>
+              </div>
+          </div>
+      </div>
+    </section>
+
+    <!-- Accordion -->
+    <section class="container text-center p-5">
+        <div class="row justify-content-center">
+            <div class="accordion col-sm-12" id="accordionExample">
+                <div class="card">
+                    <div class="card-header" id="headingOne">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                                aria-expanded="true" aria-controls="collapseOne" style="font-size:4rem">
+                                Paquete #1
+                            </button>
+                        </h2>
+                    </div>
+
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                        data-parent="#accordionExample">
+                        <div class="card-body" style="font-size:1.5rem">
+                            En este paquete te vengo ofreciendo, salida al parque justo a 2 acompañantes caninos, los cuales seran transportados hacia un parque, en donde podran correr, disfrutar de la vista y algunos premios, en cuanto a usted. Recibira una dosis de amor, con porciones extra y un desayuno tipo picnick.
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header" id="headingTwo">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style="font-size:4rem">
+                                Paquete #2
+                            </button>
+                        </h2>
+                    </div>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                        <div class="card-body" style="font-size:1.5rem">
+                            En este paquete te vengo manejando, una salida nocturna para que dejemos de sentirnos viejos y disfrutar de la vida, baile y cena al ritmo de las tarolas. 
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header" id="headingThree">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style="font-size:4rem">
+                                Paquete #3
+                            </button>
+                        </h2>
+                    </div>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                        data-parent="#accordionExample">
+                        <div class="card-body" style="font-size:1.5rem">
+                            En este ultimo paquete manejaremos una oferta especial, en esos dias dificiles en donde esta hermosa princesa se sienta perdida, se le viene ofreciendo el paquete de un dìa exclisivo para ella. Sin limites, comida, baile, nadita, amor, diversion y ternura en todo lo que ella quiera.
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-dark text-white  py-5 mt-5">
+      <div class="container">
+          <div class="row " >
+              <div class="col-md-12 text-center display-4">
+                Gracias por tantos momentos Juntos
+              </div>
+          </div>
+      </div>
+    </section>
+
+    <main class="seccion contenedor">
+      <div class="content-all">
+          <div class="content-carrousel">
+              <figure><img src="../../public/img/metro.jpg"></figure>
+              <figure><img src="../../public/img/Caballos.jpg"></figure>
+              <figure><img src="../../public/img/Dinamita.jpg"></figure>
+              <figure><img src="../../public/img/artes.jpg"></figure>
+              <figure><img src="../../public/img/Fiesta.jpg"></figure>
+              <figure><img src="../../public/img/FiestaSarita.jpg"></figure>
+              <figure><img src="../../public/img/Helados.jpg"></figure>
+              <figure><img src="../../public/img/Jaguar.jpg"></figure>
+              <figure><img src="../../public/img/Sarita.jpg"></figure>
+          </div>
+      </div>
+    </main>
+    
   </div>
 </template>
 
@@ -111,20 +185,151 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+ 
+  .side-header {
+    background-color: #333333;
+  }
+
+  .contenedor {
+    max-width: 120rem; /* 120rem = 1200px*/
+    margin: 0 auto;
+  }
+
+  .contenido-header{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.contenido-header h1{
+    color: #ffffff;
+    padding-bottom: 2rem;
+    max-width: 60rem;
+    line-height: 2; /* Interlineado*/
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.barra {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 1.5rem;
 }
-a {
-  color: #42b983;
+.navegacion a{
+    color: #ffffff;
+    text-decoration: none;
+    font-size: 1.8rem;
+    margin-right: 2rem;
+
 }
+
+.navegacion a:last-of-type{
+    margin: 0;
+}
+/****Background********* */
+.side-header {
+    background-color: #333333;
+    /* padding: 0 0 0 0; */
+}
+
+.side-header.inicio {
+    background-image: url('../../public/img//Graduacion.jpeg') ;
+    background-position: 100% 35%;
+    background-size: cover;
+    height: 80vh;
+    min-height: 60rem;
+}
+
+.contenido-header{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.contenido-header h1{
+    color: #ffffff;
+    padding-bottom: 2rem;
+    max-width: 60rem;
+    line-height: 2; /* Interlineado*/
+}
+
+/****Carrucel******/
+.section{
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+}
+
+.contenedor {
+    max-width: 120rem; /* 120rem = 1200px*/
+    margin: 0 auto;
+}
+
+.content-all{
+    width: 210px;
+    margin: auto;
+    perspective: 800px;
+    position: relative;
+    margin-top: 50px;
+}
+
+.content-carrousel{
+    width: 100%;
+    position: absolute;
+    animation: rotar 15s infinite linear;
+    transform-style: preserve-3d;
+}
+
+.content-carrousel:hover{
+    animation-play-state: paused;
+    cursor: pointer;
+}
+
+
+.content-carrousel figure{
+    width: 100%;
+    height: 120px;
+    overflow: hidden;
+    position: absolute;
+    box-shadow: 0px 0px 20px 0px black;
+    transition: all 300ms;
+    
+}
+
+.content-carrousel figure:hover{
+    box-shadow: 0px 0px 0px 0px black;
+    transition: all 300ms;
+}
+
+.content-carrousel figure:nth-child(1){transform: rotateY(0deg) translateZ(300px);}
+.content-carrousel figure:nth-child(2){transform: rotateY(40deg) translateZ(300px);}
+.content-carrousel figure:nth-child(3){transform: rotateY(80deg) translateZ(300px);}
+.content-carrousel figure:nth-child(4){transform: rotateY(120deg) translateZ(300px);}
+.content-carrousel figure:nth-child(5){transform: rotateY(160deg) translateZ(300px);}
+.content-carrousel figure:nth-child(6){transform: rotateY(200deg) translateZ(300px);}
+.content-carrousel figure:nth-child(7){transform: rotateY(240deg) translateZ(300px);}
+.content-carrousel figure:nth-child(8){transform: rotateY(280deg) translateZ(300px);}
+.content-carrousel figure:nth-child(9){transform: rotateY(320deg) translateZ(300px);}
+.content-carrousel figure:nth-child(10){transform: rotateY(360deg) translateZ(300px);}
+
+.content-carrousel img{
+    width: 100%;
+    transition: all 300ms;
+}
+
+.content-carrousel img:hover{
+    transform: scale(1.2);
+    transition: all 300ms;
+}
+
+
+@keyframes rotar{
+    from{
+        transform: rotateY(0deg);
+    }to{
+        transform: rotateY(360deg);
+    }
+}
+
 </style>
